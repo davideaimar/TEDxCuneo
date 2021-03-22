1b95e11a29aa60557ce4963f3ae78a56f2d755ed
Davide Aimar <davide.aimar99@gmail.com>
2021-03-22 19:15:12 +0100

    Init

---
+++ .eleventy.js
@@ -0,0 +1,49 @@
+const htmlmin = require('html-minifier')
+const { DateTime } = require("luxon");
+
+const now = String(Date.now())
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setUseGitIgnore(false)
+
+  eleventyConfig.addWatchTarget('./_tmp/style.css')
+
+  eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
+
+  eleventyConfig.addPassthroughCopy({
+    './node_modules/alpinejs/dist/alpine.js': './js/alpine.js',
+  })
+
+  eleventyConfig.addShortcode('version', function () {
+    return now
+  })
+
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  })
+
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (
+      process.env.ELEVENTY_PRODUCTION &&
+      outputPath &&
+      outputPath.endsWith('.html')
+    ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      })
+      return minified
+    }
+
+    return content
+  })
+
+  return {
+    dir: {
+        input: "src",
+        output: "_site",
+        layouts: '_includes/layouts'
+    }
+};
+}
---
+++ src/_data/site.js
@@ -0,0 +1,3 @@
+module.exports =  {
+    "url": "https://www.tedxcuneo.it"
+}
